b69b1beb8be4e6897f200815b86e8227dcfb6e9d
Jeff Langston <jrlangston@gmail.com>
2020-05-25 13:49:37 -0600

    exporting other axios interfaces

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,23 @@ declare module "redaxios" {
 	const redaxios: AxiosStatic;
 	export default redaxios;
 }
+
+export {
+	AxiosTransformer,
+	AxiosAdapter,
+	AxiosBasicCredentials,
+	AxiosProxyConfig,
+	AxiosRequestConfig,
+	AxiosResponse,
+	AxiosError,
+	AxiosPromise,
+	CancelStatic,
+	Cancel,
+	Canceler,
+	CancelTokenStatic,
+	CancelToken,
+	CancelTokenSource,
+	AxiosInterceptorManager,
+	AxiosInstance,
+	AxiosStatic,
+} from "axios";
